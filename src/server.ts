764f7cbff51b432ce5c19a0e288b0464088e878c
Ms.P <133916982+Mamatankane-Future@users.noreply.github.com>
2023-12-29 20:38:40 +0200

    typescript-express-starter

---
+++ src/server.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+const port = 5000;
+const app = express();
+app.get('/', (req, res) => { res.send('Hello, world!'); });
+app.listen(port, () => { console.log('Listening to port: ' + port); });
+
